dd538acff3fc04951857a17d1fe5896bc0d38f87
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-10-23 18:11:13 +0900

    1b

--- a/xxx.ts
+++ a/xxx.ts
@@ -2,7 +2,7 @@
  * @todo Take over the world
  * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
  */
-function ruleOverPunyHumans () {
+function xxxxxxxxxx () {
   // We must strategize beep boop
   // We must strategize beep boop
   // We must strategize beep boop
